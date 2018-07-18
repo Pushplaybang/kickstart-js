536c077c1b409cbac4b46351992f27bd07dd4865
pushplaybang <pushplaybang@gmail.com>
2018-07-18 23:38:43 +0200

    eslint/prettier formatting

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,11 +1,11 @@
-import multiply from "../src/index";
+import multiply from '../src/index';
 
-test("should run, and show tests are being parsed", () => {
-  const testsAre = "running";
-  expect(testsAre).toBe("running");
+test('should run, and show tests are being parsed', () => {
+  const testsAre = 'running';
+  expect(testsAre).toBe('running');
 });
 
-test("should test function", () => {
+test('should test function', () => {
   const answer = multiply(2);
   expect(answer).toBe(4);
 });
